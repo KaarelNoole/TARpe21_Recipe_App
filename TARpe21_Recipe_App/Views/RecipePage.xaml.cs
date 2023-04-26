daae2118e93ababf321536ebed977f8f4c800ebd
KaarelNoole <kaarelnoole@gmail.com>
2023-04-26 15:41:22 +0300

    Add RecipePage, in Progress

--- TARpe21_Recipe_App/Views/RecipePage.xaml.cs
+++ TARpe21_Recipe_App/Views/RecipePage.xaml.cs
@@ -1,3 +1,5 @@
+using System.Collections.ObjectModel;
+
 namespace Views;
 
 public partial class RecipePage : ContentPage
@@ -6,4 +8,22 @@ public partial class RecipePage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	protected override void OnAppearing()
+	{
+		base.OnAppearing();
+
+		//lstAllRecipe.ItemsSource = RecipesService.GetAllRecipes();
+	}
+
+	//async void Recipe_SelectionChanged(System.Object sender, Microsoft.Maui.Controls.SelectionChangedEventArgs e)
+	//{
+		//await Navigation.PushAsync(new RecipeDetailsPage(e CurrentSelection.First() as Recipe));
+	//}
+
+	private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+	{
+		//var recipes = new ObservableCollection<Recipe>();
+	}
+
 }
